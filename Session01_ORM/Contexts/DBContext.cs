77c89d8463e4dd7cd9a33220159df2aa7b9a8a57
Salma Reda <salmaredass29@gmail.com>
2025-09-09 07:57:42 +0300

    Add project files.

---
+++ Session01_ORM/Contexts/DBContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Session01_ORM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session01_ORM.Contexts
+{
+    public class CompanyContext : DbContext
+
+    {
+        public CompanyContext():base()
+            {
+            }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+      
+        {
+            optionsBuilder.UseSqlServer("server=.;database=CompanyG01;Trusted_Connection=True;TrustServerCertificate=true;");
+
+        }
+        public DbSet <Employee> Employees { get; set; }
+
+    }
+}
---
+++ Session01_ORM/Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session01_ORM.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        [MaxLength(100)]
+        [MinLength(1)]
+        [Length(1, 100)]
+        public string? Name { get; set; }
+      
+        [Required]
+        [Range(20,90)]
+        [AllowedValues(20,40,60)]
+        [DeniedValues(30,67)]
+        public int Age { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public string email { get; set; }
+        [DefaultValue(3000)]
+        [DataType(DataType.Currency)]
+        public decimal salary { get; set; }
+    }
+}
